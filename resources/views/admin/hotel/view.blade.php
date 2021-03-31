c41e6e1eda508f9ccbbadf5d3f953d37a8dc242b
Amit Karmakar <amitkarmakar.dev@gmail.com>
2021-03-31 14:14:54 +0530

    Change room image view

--- resources/views/admin/hotel/view.blade.php
+++ resources/views/admin/hotel/view.blade.php
@@ -30,25 +30,19 @@
             @foreach ($hotel->rooms as $room)
             <div class="col-md-4 py-2">
                 <div class="card h-100">
-                    @if($room->images->first())
-                    <img class="card-img-top" style="height: 150px" src="{{ $room->images->first()->image_path }}"" alt="Card image cap">
-                    @else
-                    <div class="text-center">
-                        <div class="form-group">
-                            <!-- <label for="customFile">Custom File</label> -->
-                            <div class="custom-file">
-                                <input type="file" class="custom-file-input" id="customFile">
-                                <label class="custom-file-label" for="customFile"></label>
-                            </div>
-                        </div>
-                    </div>
-                    @endif
                     <div class="card-body">
                         <h4 class="text-primary">Room no: {{ $room->room_number }}</h4>
                         <hr>
                         <p>{{ $room->description }}</p>
                         <p>Maximum person allowed: {{ $room->max_person_allowed }}</p>
                         <p>Rate: {{ $room->rate }}</p>
+                        <div class="row">
+                            @foreach ($room->images as $image )
+                            <div class="col-md-4">
+                                <img class="card-img-top" src="{{ $image->image_path }}" alt="Card image cap">
+                            </div>
+                            @endforeach
+                        </div>
                     </div>
                 </div>
 
@@ -56,9 +50,8 @@
             @endforeach
             <div class="col-md-4 py-2">
                 <div class="card h-100">
-                   <div class="card-body h-100 d-flex text-center">
-                        <a href="{{ route('hotel.room.create', ['id' => $hotel->id]) }}"
-                            class="btn btn-primary justify-content-center align-self-center">Add
+                    <div class="card-body h-100 d-flex text-center">
+                        <a href="{{ route('hotel.room.create', ['id' => $hotel->id]) }}" class="btn btn-primary justify-content-center align-self-center">Add
                             Room</a>
                     </div>
                 </div>
