2380682f9e519da9059ae25aa8328746846f4457
pratap <pratap@isl.net.in>
2021-04-11 19:57:52 +0530

    frontend ui change

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,7 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Hotel;
 use App\Models\HotelRoom;
-
+use Carbon\Carbon;
+use Carbon\CarbonPeriod;
 class HomeController extends Controller
 {
     public function index(){
@@ -17,7 +18,16 @@ class HomeController extends Controller
     public function checkAvailable(Request $request){
         $input = $request->all();
         $hotels = Hotel::where('enabled',1)->get();
-        $rooms = HotelRoom::where('hotel_id',$request['hotel_id'])->get();
-        return view('frontend.check-available',compact(['input','hotels','rooms']));
+        $rooms = HotelRoom::with('images')->where('hotel_id',$request['hotel_id'])->get();
+         
+        $startDate = Carbon::createFromFormat('Y-m-d', $input['from']);
+
+        $endDate = Carbon::createFromFormat('Y-m-d', $input['to']);
+
+  
+
+        $dateRange = CarbonPeriod::create($startDate, $endDate);
+
+        return view('frontend.check-available',compact(['input','hotels','rooms','dateRange']));
     }
 }
--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -8,7 +8,7 @@
  top -->
 
 <!-- header -->
-<nav class="navbar  navbar-default" role="navigation">
+{{-- <nav class="navbar  navbar-default" role="navigation">
     <div class="container">
       <!-- Brand and toggle get grouped for better mobile display -->
       <div class="navbar-header">
@@ -33,6 +33,23 @@
         </ul>
       </div><!-- Wnavbar-collapse -->
     </div><!-- container-fluid -->
-  </nav>
+  </nav> --}}
   <!-- header -->
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+    <a class="navbar-brand" href="frontend/images/logo.png"><img src="{{asset('frontend/images/logo.png')}}"  alt="holiday crown"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
   
+    <div class="collapse navbar-collapse navbar-right" id="navbarTogglerDemo02">
+      <ul class="navbar-nav ml-auto f-color">
+        <li><a href="index.php" class="nav-item">Home </a></li>
+        <li><a href="rooms-tariff.php" class="nav-item">Rooms & Tariff</a></li>        
+        <li><a href="introduction.php" class="nav-item">Introduction</a></li>
+        <li><a href="gallery.php" class="nav-item">Gallery</a></li>
+        <li><a href="contact.php" class="nav-item">Contact</a></li>
+      </ul>
+    </div>
+    </div>
+  </nav>
