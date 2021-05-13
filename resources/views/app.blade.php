79b3cefc9dde477b1228ca66496ca7aefc7683be
Amit Karmakar <amitkarmakar.dev@gmail.com>
2021-05-13 15:08:16 +0530

    Separate includes for frontend

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,54 +5,35 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ config('app.name') }}</title>
-{{--    <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">--}}
-    <!-- Jquery -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome Icons -->
     <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css')}}">
+    <!--Bootstrap-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <!-- IonIcons -->
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="{{ asset('plugins/AdminLTE/dist/css/adminlte.min.css')}}">
     <!-- SweetAlert2 -->
     <link rel="stylesheet" href="{{ asset('plugins/sweetalert2/sweetalert2.min.css')}}">
-    <script src="{{ asset('plugins/sweetalert2/sweetalert2.all.min.js')}}"></script>
+    <!-- Date Range-->
+    <link rel="stylesheet" href="{{ asset('plugins/daterangepicker/daterangepicker.css')}}">
+    <link rel="stylesheet" href="{{ asset('plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css')}}">
+
+    <!-- REQUIRED SCRIPTS -->
+    <!-- jQuery -->
+    <script src="{{ asset('plugins/jquery/jquery.min.js')}}"></script>
     <!-- Bootstrap -->
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <!-- gallery -->
-    <link rel="stylesheet" href="{{ asset('plugins/frontend/assets/gallery/blueimp-gallery.min.css')}}">
-    <!-- uniform -->
-    <link type="text/css" rel="stylesheet" href="{{ asset('plugins/frontend/assets/uniform/css/uniform.default.min.css')}}"/>
-    <!-- animate.css -->
-    <link rel="stylesheet" href="{{ asset('plugins/frontend/assets/wow/animate.css')}}"/>
-    <!-- Frontend styles -->
-    <link rel="stylesheet" href="{{asset('plugins/frontend/assets/style.css')}}">
-    <link rel="stylesheet" href="{{ asset('plugins/frontend/css/style.css')}}"/>
-<!-- wow script -->
-    <script src="{{ asset('plugins/frontend/assets/wow/wow.min.js')}}"></script>
-    <!-- uniform -->
-    <script src="{{ asset('plugins/frontend/assets/uniform/js/jquery.uniform.js')}}"></script>
-    <!-- jquery mobile -->
-    <script src="{{ asset('plugins/frontend/assets/mobile/touchSwipe.min.js')}}"></script>
-    <!-- jquery mobile -->
-    <script src="{{ asset('plugins/frontend/assets/respond/respond.js')}}"></script>
-    <!-- gallery -->
-    <script src="{{ asset('plugins/frontend/assets/gallery/jquery.blueimp-gallery.min.js')}}"></script>
-    <!-- custom script -->
-    <script src="{{ asset('plugins/frontend/assets/script.js')}}"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <!-- VueJS -->
     <script src="https://cdn.jsdelivr.net/npm/vue@2.6.12"></script>
+    <!-- SweetAlert -->
+    <script src="{{ asset('plugins/sweetalert2/sweetalert2.all.min.js')}}"></script>
     <!-- Moment.js -->
     <script src="{{ asset('plugins/moment/moment.min.js')}}"></script>
-    <!-- DateRangePicker -->
+    <!-- Date Range-->
     <script src="{{ asset('plugins/daterangepicker/daterangepicker.js')}}"></script>
-    <link rel="stylesheet" href="{{ asset('plugins/daterangepicker/daterangepicker.css') }}">
-    <script src="{{ asset('plugins/bs-custom-file-input/bs-custom-file-input.min.js') }}"></script>
-    <!-- Tempusdominus Bootstrap 4 -->
     <script src="{{ asset('plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js')}}"></script>
+    @yield('plugins')
 </head>
 
 <body class="hold-transition sidebar-mini">
