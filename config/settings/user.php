9f577c185e8350d0a6a42c7c7786a6250fec6646
Amit Karmakar <amitkarmakar.dev@gmail.com>
2021-06-10 07:25:53 +0530

    Fix user creation validation rule

--- config/settings/user.php
+++ config/settings/user.php
@@ -6,7 +6,6 @@ return [
         'name' => ['required', 'string', 'max:255'],
         'phone_number' => ['required','unique:users'],
         'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-        'password' => ['required', 'string', 'min:6'],
     ],
 
     'update_validation_rules' => [
